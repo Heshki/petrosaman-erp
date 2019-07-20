f2577ebc603a3e030d3c027ca888406aed2dfd96
Seyed Morteza Mahdavi <mahdavy1456@yahoo.com>
2019-07-20 22:34:25 +0430

    حذف ?> از آخر فایل

--- include/api/sms.php
+++ include/api/sms.php
@@ -1,6 +1,6 @@
 <?php
 function send_sms($number, $r_status){
-	$url = "37.130.202.188/services.jspd";	
+	$url = "37.130.202.188/services.jspd";
 	$rcpt_nm = array($number);
 	$param = array(
 		'uname' => '',
@@ -9,15 +9,14 @@ function send_sms($number, $r_status){
 		'message' => 'با سلام تلفن همراه شما ' . $r_status . ' است باتشکر موبایل جمهوری',
 		'to' => json_encode($rcpt_nm),
 		'op' => 'send'
-	);				
-	$handler = curl_init($url);             
+	);
+	$handler = curl_init($url);
 	curl_setopt($handler, CURLOPT_CUSTOMREQUEST, "POST");
-	curl_setopt($handler, CURLOPT_POSTFIELDS, $param);                       
+	curl_setopt($handler, CURLOPT_POSTFIELDS, $param);
 	curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
 	$response2 = curl_exec($handler);
-		
+
 	$response2 = json_decode($response2);
 	$res_code = $response2[0];
-	$res_data = $response2[1];	
+	$res_data = $response2[1];
 }
-?>
