17b4e3aac2dc9bf80bcfa9cd570dbef2923d4764
mrm-esmaili <51052610+mrm-esmaili@users.noreply.github.com>
2019-07-20 21:33:39 +0430

    اطلاعات اتصال به دیتابیس

---
+++ include/data/config.php
@@ -0,0 +1,22 @@
+<?php
+$src = $_SERVER['DOCUMENT_ROOT'] . "/gt-config.gt";
+$myfile = fopen( $src, "r" ) or die("Unable to open file!");
+$data = fread($myfile,filesize($src));
+fclose($myfile);
+
+preg_match('/"([^"]+)"/', explode(",", $data)[0], $hostArray);
+$GLOBALS['host'] = $hostArray[1];
+
+preg_match('/"([^"]+)"/', explode(",", $data)[1], $db_nameArray);
+$GLOBALS['db_name'] = $db_nameArray[1];
+
+preg_match('/"([^"]+)"/', explode(",", $data)[2], $db_userArray);
+$GLOBALS['db_user'] = $db_userArray[1];
+
+preg_match('/"([^"]+)"/', explode(",", $data)[3], $db_user_passArray);
+if(!empty($db_user_passArray[1])){
+	$GLOBALS['db_user_pass'] = $db_user_passArray[1];
+}else{
+	$GLOBALS['db_user_pass'] = "";
+}
+?>
