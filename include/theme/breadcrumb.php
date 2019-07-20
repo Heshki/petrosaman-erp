b64b7190ef507684777dddfe583920b0bacd8687
mrm-esmaili <51052610+mrm-esmaili@users.noreply.github.com>
2019-07-20 21:34:48 +0430

    breadcrump

---
+++ include/theme/breadcrumb.php
@@ -0,0 +1,21 @@
+<?php
+function breadcrumb($title){ ?>
+	<section class="content-header">
+		<div id="page-wrapper">
+			<div class="row">
+				<div class="col-lg-12">
+					<h1 class="page-header"><?php echo $title; ?></h1>
+				</div>
+			</div>
+		</div>
+		<?php
+			$_SESSION['user_interactions'][] = $_SERVER['HTTP_REFERER'];
+			$previous_page = end($_SESSION['user_interactions']);
+		?>
+		<ol class="breadcrumb">
+			<li><a href="<?php get_url(); ?>index.php" class="btn btn-success" style="color:white;"><i class="fa fa-dashboard"></i> خانه</a></li>
+			<li><a href="<?php echo $previous_page; ?>" class="btn btn-primary" style="color:white;">بازگشت</a></li>
+		</ol>
+	</section>
+<?php
+}
